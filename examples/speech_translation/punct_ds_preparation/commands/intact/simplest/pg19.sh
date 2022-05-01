fd6ad576d2c84bfd21a836942be6fd1b68736610
PeganovAnton <peganoff2@mail.ru>
2022-05-01 16:40:52 +0300

    Add intact command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/commands/intact/simplest/pg19.sh
@@ -0,0 +1,16 @@
+python prepare_big_data_for_punctuation_capitalization_task_simple.py \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/intact/simplest/pg19_1.5.2022 \
+  --corpus_types pg19 \
+  --create_model_input \
+  --bert_labels \
+  --autoregressive_labels \
+  --sequence_length_range 3 128 \
+  --allowed_punctuation '.,?' \
+  --only_first_punctuation_character_after_word_in_autoregressive \
+  --no_label_if_all_characters_are_upper_case \
+  --input_files_or_dirs /media/apeganov/DATA/deepmind-gutenberg/train \
+  --num_jobs 24 \
+  --dev_size 0 \
+  --test_size 0 \
+  --intact_sentences \
+  --use_nltk_sentence_splitting \
