c94f4c33469510baea2decf87a88c5e7da659bfc
fayejf <fayejf07@gmail.com>
2022-07-12 14:18:40 -0700

    update
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- examples/asr/speech_classification/vad_infer.py
+++ examples/asr/speech_classification/vad_infer.py
@@ -32,7 +32,7 @@ python vad_infer.py --config-path="../conf/vad" --config-name="vad_inference_pos
 """
 import json
 import os
-
+import time
 import torch
 
 from nemo.collections.asr.parts.utils.speaker_utils import write_rttm2manifest
@@ -122,7 +122,7 @@ def main(cfg):
     logging.info(
         f"Finish generating VAD frame level prediction with window_length_in_sec={cfg.vad.parameters.window_length_in_sec} and shift_length_in_sec={cfg.vad.parameters.shift_length_in_sec}"
     )
-
+    frame_length_in_sec = cfg.vad.parameters.shift_length_in_sec
     # overlap smoothing filter
     if cfg.gen_overlap_seq:
         # Generate predictions with overlapping input segments. Then a smoothing filter is applied to decide the label for a frame spanned by multiple segments.
@@ -141,6 +141,7 @@ def main(cfg):
             f"Finish generating predictions with overlapping input segments with smoothing_method={cfg.vad.parameters.smoothing} and overlap={cfg.vad.parameters.overlap}"
         )
         pred_dir = smoothing_pred_dir
+        frame_length_in_sec = 0.01
 
     # postprocessing and generate speech segments
     if cfg.gen_seg_table:
@@ -148,7 +149,7 @@ def main(cfg):
         table_out_dir = generate_vad_segment_table(
             vad_pred_dir=pred_dir,
             postprocessing_params=cfg.vad.parameters.postprocessing,
-            shift_length_in_sec=cfg.vad.parameters.shift_length_in_sec,
+            frame_length_in_sec=frame_length_in_sec,
             num_workers=cfg.num_workers,
             out_dir=cfg.table_out_dir,
         )
@@ -159,7 +160,6 @@ def main(cfg):
     if cfg.write_to_manifest:
         for i in key_meta_map:
             key_meta_map[i]['rttm_filepath'] = os.path.join(table_out_dir, i + ".txt")
-
         if not cfg.out_manifest_filepath:
             out_manifest_filepath = "vad_out.json"
         else:
@@ -169,4 +169,7 @@ def main(cfg):
 
 
 if __name__ == '__main__':
+    start = time.time()
     main()
+    end = time.time()
+    print(f"take {end-start} time")
