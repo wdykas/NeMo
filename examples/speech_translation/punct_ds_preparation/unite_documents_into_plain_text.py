eab7013b7871dab0d3766fbb454dedfdcc9696dd
PeganovAnton <peganoff2@mail.ru>
2022-07-16 19:37:40 +0300

    Add option of passing a directory instead of files as inputs
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/unite_documents_into_plain_text.py
+++ examples/speech_translation/punct_ds_preparation/unite_documents_into_plain_text.py
@@ -10,18 +10,24 @@ MARKUP = re.compile('^<doc/>.*\n|^<doc docid=.*\n', flags=re.MULTILINE)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--inputs", nargs="+", type=Path, required=True)
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument("--inputs", nargs="+", type=Path)
+    input_group.add_argument("--input_dir", type=Path)
     parser.add_argument("--output", type=Path, required=True)
     args = parser.parse_args()
     args.output = args.output.expanduser()
-    args.inputs = [elem.expanduser() for elem in args.inputs]
+    if args.inputs is None:
+        args.input_dir = args.input_dir.expanduser()
+    else:
+        args.inputs = [elem.expanduser() for elem in args.inputs]
     return args
 
 
 def main() -> None:
     args = parse_args()
+    input_files = list(args.input_dir.iterdir()) if args.inputs is None else args.inputs
     with args.output.open('w') as out_f:
-        for input_file in tqdm(args.inputs, desc="Uniting documents", unit="file"):
+        for input_file in tqdm(input_files, desc="Uniting documents", unit="file"):
             with input_file.open() as in_f:
                 out_f.write(MARKUP.sub('', in_f.read()))
 
