f552ab1825bcefd3afee53d7a1715319ead36f87
PeganovAnton <peganoff2@mail.ru>
2022-07-16 19:17:44 +0300

    Add unite documents script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/unite_documents_into_plain_text.py
@@ -0,0 +1,28 @@
+import argparse
+import re
+from pathlib import Path
+
+
+MARKUP = re.compile('^<doc/>.*\n|^<doc docid=.*\n', flags=re.MULTILINE)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputs", nargs="+", type=Path, required=True)
+    parser.add_argument("--output", type=Path, required=True)
+    args = parser.parse_args()
+    args.output = args.output.expanduser()
+    args.inputs = [elem.expanduser() for elem in args.inputs]
+    return args
+
+
+def main() -> None:
+    args = parse_args()
+    with args.output.open('w') as out_f:
+        for input_file in args.inputs:
+            with input_file.open() as in_f:
+                out_f.write(MARKUP.sub('', in_f.read()))
+
+
+if __name__ == "__main__":
+    main()
