93831f0aa1df6aac5197c1b4bb3e38494286e2a1
PeganovAnton <peganoff2@mail.ru>
2022-07-16 15:41:40 +0300

    Add draft of cut_segments.py script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/cut_segments.py
+++ examples/speech_translation/punct_ds_preparation/cut_segments.py
@@ -15,10 +15,11 @@ BUFF_SIZE = 2 ** 20
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--input_file", type=Path)
-    parser.add_argument("--output_file", type=Path)
+    parser.add_argument("--input_file", type=Path, required=True)
+    parser.add_argument("--output_file", type=Path, required=True)
     parser.add_argument("--start_length", type=int, default=3)
     parser.add_argument("--end_length", type=int, default=128)
+    parser.add_argument("--num_passes_through_dataset", type=int, default=1)
     args = parser.parse_args()
     args.input_file = args.input_file.expanduser()
     args.output_file = args.output_file.expanduser()
@@ -39,28 +40,30 @@ def main() -> None:
     args = parse_args()
     perm = list(range(args.start_length, args.end_length))
     random.shuffle(perm)
-    p_i = 0
-    b_i = 0
-    with args.input_file.open(buffering=BUFF_SIZE) as in_f, args.output_file.open('w', buffering=BUFF_SIZE) as out_f:
-        buff = in_f.read(BUFF_SIZE).replace('\n', ' ')
-        while True:
-            if p_i == len(perm):
-                p_i = 0
-                random.shuffle(perm)
-            if args.end_length >= len(small.WORD_WITH_PRECEDING_AND_FOLLOWING_PUNCTUATION.findall(buff[b_i:])):
-                buff = buff[b_i:]
-                b_i = 0
-                buff += in_f.read(BUFF_SIZE).replace('\n', ' ')
-            found_required_length = False
-            for m_i, m in enumerate(small.WORD_WITH_PRECEDING_AND_FOLLOWING_PUNCTUATION.finditer(buff[b_i:])):
-                if m_i >= perm[p_i] - 1:
-                    out_f.write(buff[b_i : b_i + m.span()[1]] + '\n')
-                    b_i = b_i + m.span()[1]
-                    found_required_length = True
-                    break
-            if not found_required_length:
-                out_f.write(buff[b_i :])
-            p_i += 1
+    p_i = 0  # index of segment length in permutation `perm`
+    b_i = 0  #
+    with args.output_file.open('w', buffering=BUFF_SIZE) as out_f:
+        for _ in range(args.num_passes_throuh_dataset):
+            with args.input_file.open(buffering=BUFF_SIZE) as in_f:
+                buff = in_f.read(BUFF_SIZE).replace('\n', ' ')
+                while True:
+                    if p_i == len(perm):
+                        p_i = 0
+                        random.shuffle(perm)
+                    if args.end_length >= len(small.WORD_WITH_PRECEDING_AND_FOLLOWING_PUNCTUATION.findall(buff[b_i:])):
+                        buff = buff[b_i:]
+                        b_i = 0
+                        buff += in_f.read(BUFF_SIZE).replace('\n', ' ')
+                    found_required_length = False
+                    for m_i, m in enumerate(small.WORD_WITH_PRECEDING_AND_FOLLOWING_PUNCTUATION.finditer(buff[b_i:])):
+                        if m_i >= perm[p_i] - 1:
+                            out_f.write(buff[b_i : b_i + m.span()[1]] + '\n')
+                            b_i = b_i + m.span()[1]
+                            found_required_length = True
+                            break
+                    if not found_required_length:
+                        out_f.write(buff[b_i :])
+                    p_i += 1
 
 
 if __name__ == "__main__":
