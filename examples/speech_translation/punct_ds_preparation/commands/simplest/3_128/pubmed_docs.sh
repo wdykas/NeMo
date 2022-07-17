1a4bbab41d4167d66419d93fca7c84ae13e0de72
PeganovAnton <peganoff2@mail.ru>
2022-07-17 14:54:59 +0300

    fix: add inputs to pubmed_docs.sh script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/commands/simplest/3_128/pubmed_docs.sh
+++ examples/speech_translation/punct_ds_preparation/commands/simplest/3_128/pubmed_docs.sh
@@ -2,5 +2,6 @@ python prepare_big_data_for_punctuation_capitalization_task_simple.py \
   --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/pubmed_x1_17.07.2022 \
   --stop_after_documents_preparation \
   --corpus_types pubmed \
+  --input_files_or_dirs /media/apeganov/DATA/pubmed_OA_commercial/extracted \
   --num_jobs 24 \
   --num_passes_through_dataset 1
