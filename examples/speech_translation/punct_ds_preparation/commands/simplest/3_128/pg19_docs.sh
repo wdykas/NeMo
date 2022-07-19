e6d2ba6b593dace340ed803fe7af9e959f3adb17
PeganovAnton <peganoff2@mail.ru>
2022-07-19 15:24:13 +0300

    Add only docs preparation script for PG19
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/commands/simplest/3_128/pg19_docs.sh
@@ -0,0 +1,6 @@
+python prepare_big_data_for_punctuation_capitalization_task_simple.py \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/pg19_x1_19.07.2022 \
+  --corpus_types pg19 \
+  --input_files_or_dirs /media/apeganov/DATA/deepmind-gutenberg/train \
+  --num_jobs 24 \
+  --stop_after_documents_preparation
