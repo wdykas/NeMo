7cc0dd64fc5f6f9aff6a93ccb3daf9929154e576
PeganovAnton <peganoff2@mail.ru>
2022-07-20 22:38:20 +0300

    Add tqdm
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/nltk_sent_tokenize.py
+++ examples/speech_translation/punct_ds_preparation/nltk_sent_tokenize.py
@@ -1,8 +1,11 @@
 import argparse
+import os
 from pathlib import Path
-from typing import Generator
+from subprocess import PIPE, run
+from typing import Generator, Union
 
 import nltk
+from tqdm import tqdm
 
 import prepare_small_data_for_punctuation_capitalization_task as small
 
@@ -17,9 +20,20 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
+def get_num_lines(input_file: Union[str, os.PathLike]) -> int:
+    result = run(['wc', '-l', str(input_file)], stdout=PIPE, stderr=PIPE)
+    if not result:
+        raise ValueError(
+            f"Bash command `wc -l {input_file}` returned and empty string. "
+            f"Possibly, file {input_file} does not exist."
+        )
+    return int(result.stdout.decode('utf-8').split()[0])
+
+
 def sentence_generator(input_file: Path) -> Generator[str, None, None]:
+    num_lines = get_num_lines(input_file)
     with input_file.open() as in_f:
-        for line in in_f:
+        for line in tqdm(in_f, total=num_lines, unit="line", desc="Tokenizing into sentences"):
             for sent in nltk.sent_tokenize(line):
                 if len(small.WORD.findall(sent)) > 0:
                     yield sent.rstrip()
