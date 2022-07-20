de1a7829e6e8bdbf0b1f24768bdfb7240efcf3f4
PeganovAnton <peganoff2@mail.ru>
2022-07-20 22:17:32 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/join_sentences_with_ratios.py
+++ examples/speech_translation/punct_ds_preparation/join_sentences_with_ratios.py
@@ -43,7 +43,7 @@ def main() -> None:
                 current_segment = ""
                 n_sent_in_current_segment = 0
                 n_words_in_current_segment = 0
-            elif n_sent_in_current_segment >= args.counts[segment_num_sent_i]:
+            elif n_sent_in_current_segment >= num_sent_in_segment[segment_num_sent_i]:
                 f.write(current_segment + sent + '\n')
                 current_segment = ""
                 n_sent_in_current_segment = 0
