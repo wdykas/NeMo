757274b7a314e0c5376af15b8a18d1d368332371
PeganovAnton <peganoff2@mail.ru>
2022-07-26 10:15:19 +0300

    Change image
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/slurm_commands/commercial_10000.sh
+++ examples/nlp/token_classification/slurm_commands/commercial_10000.sh
@@ -12,7 +12,7 @@
 #SBATCH --ntasks-per-node=16     # n tasks per machine (one task per gpu) <required>
 set -x
 USERID='apeganov'
-CONTAINER="nvcr.io/nvidia/nemo:22.05"
+CONTAINER="gitlab-master.nvidia.com/apeganov/punctuation-and-capitalization:latest"
 WANDB="${wandb}" # replace with your own WandB API key
 
 # Training - we want to train for 300B tokens with a global batch size of at least 1M tokens
