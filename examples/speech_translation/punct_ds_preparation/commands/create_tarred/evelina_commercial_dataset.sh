d8989b424bb0f0d75efd1eafebd49eaf1bd177a2
PeganovAnton <peganoff2@mail.ru>
2022-07-29 09:20:11 +0300

    Update ds name
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/commands/create_tarred/evelina_commercial_dataset.sh
+++ examples/speech_translation/punct_ds_preparation/commands/create_tarred/evelina_commercial_dataset.sh
@@ -1,5 +1,5 @@
 tokens_in_batch="$1"
-ds_name=commercial
+ds_name="commercial_${tokens_in_batch}"
 python ~/NeMo/examples/nlp/token_classification/data/create_punctuation_capitalization_tarred_dataset.py \
   --text "/media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/${ds_name}/train/input.txt" \
   --labels "/media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/${ds_name}/train/bert_labels.txt" \
