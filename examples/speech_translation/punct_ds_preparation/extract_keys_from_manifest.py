9a39f62550abf93b90cac659941dbc4c7a3afbe0
PeganovAnton <peganoff2@mail.ru>
2022-08-16 16:51:50 +0300

    Add draft of manifest extract script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/extract_keys_from_manifest.py
@@ -0,0 +1,29 @@
+import argparse
+import json
+
+from pathlib import Path
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--input", "-i", required=True, type=Path)
+    parser.add_argument("--output", "-o", required=True, type=Path)
+    parser.add_argument("--key", required=True)
+    args = parser.parse_args()
+    args.input = args.input.expanduser()
+    args.output = args.output.expanduser()
+    return args
+
+
+def main() -> None:
+    args = parse_args()
+    with args.input.open() as in_f:
+        lines = in_f.readlines()
+    extracted = [json.loads(line)[args.key].strip() for line in lines]
+    with args.output.open('w') as out_f:
+        for line in extracted:
+            out_f.write(line + '\n')
+
+
+if __name__ == "__main__":
+    main()
