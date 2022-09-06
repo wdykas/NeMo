f3f6798ab1975ef435404c80f81d8a0f32dc8922
PeganovAnton <peganoff2@mail.ru>
2022-09-06 14:28:49 +0300

    Backup progress on adding NGC scripts
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/nlp/language_modeling/ngc_commands/command_commercial_punctuation_and_capitalization_bert.sh
@@ -0,0 +1,51 @@
+set -x
+WANDB_API_KEY="$1"
+exp_name="$2"
+gpus="$3"
+
+set -e -x
+result_dir=/result/nemo_experiments
+mkdir -p "${result_dir}"
+cd /workspace/NeMo
+git pull
+bash reinstall.sh
+cd examples/nlp/token_classification
+wandb login ${WANDB_API_KEY}
+python punctuation_capitalization_train_evaluate.py --config-path=conf \
+    --config-name commercial_bs320k_steps400k \
+    exp_manager.wandb_logger_kwargs.name=${exp_name} \
+    +exp_manager.explicit_log_dir="${result_dir}" \
+    trainer.devices=${gpus} \
+    ~trainer.max_steps \
+    trainer.max_epochs=3 \
+    model.train_ds.ds_item=/data/train_bert_tarred_10000 \
+    +model.train_ds.label_info_save_dir=/raid/train_labels \
+    model.validation_ds.ds_item=[/data/europarl_segments_dev,\
+/data/europarl_sentences_dev,\
+/data/google_segments_dev,\
+/data/google_sentences_dev,\
+/data/pg19_segments_dev,\
+/data/pg19_sentences_dev,\
+/data/pubmed_segments_dev,\
+/data/pubmed_sentences_dev,\
+/data/tatoeba_segments_dev,\
+/data/tatoeba_sentences_dev,\
+/data/un_segments_dev,\
+/data/un_sentences_dev] \
+    model.validation_ds.n_jobs=null \
+    +model.validation_ds.cache_dir=/raid/validation_ds_cache \
+    model.test_ds.ds_item=[/data/europarl_segments_test,\
+/data/europarl_sentences_test,\
+/data/google_segments_test,\
+/data/google_sentences_test,\
+/data/pg19_segments_test,\
+/data/pg19_sentences_test,\
+/data/pubmed_segments_test,\
+/data/pubmed_sentences_test,\
+/data/tatoeba_segments_test,\
+/data/tatoeba_sentences_test,\
+/data/un_segments_test,\
+/data/un_sentences_test] \
+    model.test_ds.n_jobs=null \
+    +model.test_ds.cache_dir=/raid/test_ds_cache
+set +x
---
+++ examples/nlp/language_modeling/ngc_commands/tmux_starter.sh
@@ -0,0 +1,35 @@
+script_name="$1"
+dataset_id="$2"
+WANDB_API_KEY="$3"
+exp_name="$4"
+if [ -z "$5" ]; then
+  gpus=8
+else
+  gpus="$5"
+fi
+if [ -z "$6" ]; then
+  gpu_memory=16
+else
+  gpu_memory="$6"
+fi
+
+read -r -d '' command << EOF
+set -x -e
+tmux new-session -d -s "work" 'bash' 2>&1 | tee -a /result/tmux_logs.txt
+tmux new-window -t test | tee -a /result/tmux_logs.txt
+ls /workspace/NeMo/examples/nlp/token_classification/ngc_commands/ 2>&1 | tee -a /result/tmux_logs.txt
+tmux send-keys -t "work:0.0" 'bash /workspace/NeMo/examples/nlp/token_classification/ngc_commands/${script_name} \
+  ${WANDB_API_KEY} \
+  ${exp_name} \
+  ${gpus}' Enter 2>&1 | tee -a /result/tmux_logs.txt
+sleep 1000000
+set +x +e
+EOF
+
+ngc batch run \
+  --instance "dgx1v.${gpu_memory}g.${gpus}.norm" \
+  --name "ml-model.p-tuning ${exp_name}" \
+  --image "nvcr.io/nvidian/ac-aiapps/punctuation-and-capitalization:latest" \
+  --result /result \
+  --datasetid ${dataset_id}:/data \
+  --commandline "${command}"
