4aba51253e8acb4b6ee6b2a80c1cca2c3875bd1c
wdykas <wdykas@nvidia.com>
2023-06-13 07:34:44 -0700

    small changes

--- examples/nlp/language_modeling/run_gpt.sh
+++ examples/nlp/language_modeling/run_gpt.sh
@@ -1,12 +1,12 @@
 TORCH_DISTRIBUTED_DEBUG=DETAIL HYDRA_FULL_ERROR=1 torchrun --nnode 1 --nproc_per_node 2 megatron_gpt_pretraining.py \
     trainer.devices=2 \
     model.data.data_prefix=[1,/workspace/gpt-wiki/Wikipedia_en_ftfy_id_shuf_text_document] \
-    model.tensor_model_parallel_size=1 \
+    model.tensor_model_parallel_size=2 \
     model.micro_batch_size=4 \
     model.global_batch_size=16 \
     model.data.dataloader_type=single \
     model.seed=32345435 \
     model.tokenizer.vocab_file=/workspace/gpt-wiki/gpt2-vocab.json \
     model.tokenizer.merge_file=/workspace/gpt-wiki/gpt2-merges.txt \
-    exp_manager.checkpoint_callback_params.save_nemo_on_train_end=True \
     trainer.max_steps=1000
+    #exp_manager.checkpoint_callback_params.save_nemo_on_train_end=True \
--- examples/nlp/language_modeling/run_sft.sh
+++ examples/nlp/language_modeling/run_sft.sh
@@ -1,7 +1,8 @@
-TRAIN="[/workspace/databricks-dolly-15k-output.jsonl]"
+TRAIN="[/workspace/dolly-data/databricks-dolly-15k-output.jsonl]"
 
-VALID="[/workspace/databricks-dolly-15k-output.json]"
+VALID="[/workspace/dolly-data/databricks-dolly-15k-output.jsonl]"
 
+TEST="[/workspace/dolly-data/databricks-dolly-15k-output.jsonl]"
 VALID_NAMES="[your-validation-dataset-name]"
 
 CONCAT_SAMPLING_PROBS="[1.0]"
@@ -15,7 +16,7 @@ python /workspace/sft-nemo/NeMo/examples/nlp/language_modeling/tuning/megatron_g
   trainer.max_steps=1000 \
   trainer.devices=2 \
   trainer.val_check_interval=200 \
-  model.megatron_amp_O2=True \
+  model.megatron_amp_O2=False \
   model.tensor_model_parallel_size=${TP_SIZE} \
   model.pipeline_model_parallel_size=${PP_SIZE} \
   model.optim.lr=5e-6 \
@@ -26,7 +27,7 @@ python /workspace/sft-nemo/NeMo/examples/nlp/language_modeling/tuning/megatron_g
   model.data.validation_ds.micro_batch_size=1 \
   model.data.validation_ds.global_batch_size=128 \
   model.data.validation_ds.file_names=${VALID} \
-  model.data.validation_ds.names=${VALID_NAMES} \
+  model.data.test_ds.file_names=${TEST} \
   model.data.test_ds.micro_batch_size=1 \
   model.data.test_ds.global_batch_size=128 \
   model.data.train_ds.num_workers=0 \
@@ -40,6 +41,7 @@ python /workspace/sft-nemo/NeMo/examples/nlp/language_modeling/tuning/megatron_g
   exp_manager.resume_ignore_no_checkpoint=True \
   exp_manager.create_checkpoint_callback=True \
   exp_manager.checkpoint_callback_params.monitor=validation_loss \
-  model.restore_from_path="/workspace/sft-nemo/NeMo/examples/nlp/language_modeling/nemo_experiments/megatron_gpt/checkpoints/megatron_gpt--val_loss-5.45-step-1400-consumed_samples-22384.0-last.ckpt"
-  #model.data.train_ds.concat_sampling_probabilities=${CONCAT_SAMPLING_PROBS} \
+  model.restore_from_path="/workspace/sft-nemo/NeMo/examples/nlp/language_modeling/nemo_experiments/megatron_gpt/checkpoints/megatron_gpt.nemo" \
+  model.data.train_ds.concat_sampling_probabilities=${CONCAT_SAMPLING_PROBS}
+  #model.data.validation_ds.names=${VALID_NAMES} \
 
